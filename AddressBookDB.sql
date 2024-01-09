f8275fad3a0f508f73ba4f0bf12a4c1db0a7d150
Rihal Shaikh <rihalshaikh999@gmail.com>
2024-01-09 11:47:16 +0530

    Added table description by using primary key, not null, and auto_increment

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -1,3 +1,18 @@
 -- UC1
 create database addressbookdb;
+USE AddressBookDB;
 
+-- UC2 
+create table AddressBook(
+id int PRIMARY KEY AUTO_INCREMENT,
+FirstName varchar(40) not null,
+LastName varchar(40) not null,
+adress varchar(100),
+city varchar(40),
+state varchar(40),
+zip varchar(6),
+phone varchar(20),
+email varchar(50)
+);
+DESC addressbook;
+select  * from AddressBook;
